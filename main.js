405d1184d8686cc8415b968a64d9fe080f23a3f8
aronhailu <aron.hailu@gmail.com>
2019-08-27 16:15:04 +0300

    adding Ad Link on multiple download links

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
+var URL = 'http://www.damaengineeringsolution.com';
+var windowName = 'DAMA_ENGINEERING';
+var windowWidth = '700';
+var windowHight = '600';
+
 (function() {
 
-    document.querySelector('.download').addEventListener('click', function() {
+    var downloadButton = document.querySelectorAll('.download');
+    
+    for(var i=0; i<downloadButton.length; i++) {
+        
+        downloadButton[i].addEventListener('click', function() {
 
-        var URL = 'http://www.damaengineeringsolution.com';
-        var windowName = 'DAMA_ENGINEERING';
-        var windowWidth = '700';
-        var windowHight = '600';
-        var centerLeft = (screen.width/2) - (windowWidth/2);
-        var centerTop = (screen.height/2) - (windowHight/2);
-        var windowFeatures = 'toolbar=no, location=no, directories=no, status=no, menubar=no, titlebar=no, scrollbars=no, resizable=no ';
+            var centerLeft = (screen.width/2) - (windowWidth/2);
+            var centerTop = (screen.height/2) - (windowHight/2);
+            var windowFeatures = 'toolbar=no, location=no, directories=no, status=no, menubar=no, titlebar=no, scrollbars=no, resizable=no ';
+    
+            return window.open(URL, windowName, windowFeatures + ', width=' + windowWidth + ', height=' + windowHight + ', top=' + centerTop +
+                    ', left=' + centerLeft);
+    
+       });
 
-        return window.open(URL, windowName, windowFeatures + ', width=' + windowWidth + ', height=' + windowHight + ', top=' + centerTop +
-                ', left=' + centerLeft);
+    }
 
-    });
-
-})()
+})(URL, windowName, windowWidth, windowHight);
 
 
 
