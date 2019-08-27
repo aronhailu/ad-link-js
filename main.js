bfddd9b870cf96dd968053420609220fb85ad8a2
aronhailu <aron.hailu@gmail.com>
2019-08-27 15:40:59 +0300

    adding ad link on one download button

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-var url = "http://damaengineeringsolution.com";
+(function() {
 
-window.onload = function() {
+    document.querySelector('.download').addEventListener('click', function() {
 
-    document.querySelector('#download').addEventListener('click', setTimeout(() => {
+        var URL = 'http://www.damaengineeringsolution.com';
+        var windowName = 'DAMA_ENGINEERING';
+        var windowWidth = '700';
+        var windowHight = '600';
+        var centerLeft = (screen.width/2) - (windowWidth/2);
+        var centerTop = (screen.height/2) - (windowHight/2);
+        var windowFeatures = 'toolbar=no, location=no, directories=no, status=no, menubar=no, titlebar=no, scrollbars=no, resizable=no ';
 
-        window.location = url;
+        return window.open(URL, windowName, windowFeatures + ', width=' + windowWidth + ', height=' + windowHight + ', top=' + centerTop +
+                ', left=' + centerLeft);
 
-    }, 5000))
+    });
 
-};
+})()
+
+
+
+
+
+   
